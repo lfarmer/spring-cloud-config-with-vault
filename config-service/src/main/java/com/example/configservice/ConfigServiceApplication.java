1724e387a6d539fb6d01958f116722a59b0a53b1
Lea Farmer <lea@leaf-development.co.uk>
2018-12-10 21:45:52 +0000

    Mimic cloud foundry environment
    Mimic cloud foundry environment to test out cloud connectors
    for setting acquiring vault connection information

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -1,12 +1,29 @@
 package com.example.configservice;
 
+import io.pivotal.spring.cloud.vault.service.common.VaultServiceInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
+
+    @Bean
+    @Profile("cloud")
+    public void something() {
+        System.out.println(System.getenv("VCAP_APPLICATION"));
+        System.out.println(System.getenv("VCAP_SERVICES"));
+        CloudFactory cloudFactory = new CloudFactory();
+        Cloud cloud = cloudFactory.getCloud();
+        VaultServiceInfo myService = (VaultServiceInfo) cloud.getServiceInfo("my-vault");
+        myService.getUri();
+        myService.getToken();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
